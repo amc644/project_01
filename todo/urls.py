df84d7e6dbfd1ffece9be6f8b9fe999ea3fa9cfb
amc644 <amc644@gmail.com>
2024-11-29 15:52:34 -0500

    Add todo part 2

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('', views)
+]
+
+
 '''
 urlpatterns = [
     path('', views.main, name='main'),
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Todo
+from .forms import TodoForm
 
-# Create your views here.
+def index(request):
+    item_list = Todo.objects.order_by("-date")
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        
